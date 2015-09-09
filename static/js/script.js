045c94a7cf21be2e8d52de060dd9a59c89b8e2ce
jktravis <joshua.travis@gmail.com>
2015-09-09 11:10:02 -0400

    Somewhat working.

--- static/js/script.js
+++ static/js/script.js
@@ -20,22 +20,50 @@ $(document).ready(function ()
     var breakLength = $('#breakLength');
     var sessionLength = $('#sessionLength');
 
+    var timer = $('.timer');
+
     breakLength.val(Pomodoro.breakLength);
     sessionLength.val(Pomodoro.sessionLength);
 
     breakLength.on('change', function(e)
     {
-        console.log(breakLength.val());
+        Pomodoro.setBreak(breakLength.val());
+        console.log(Pomodoro.breakLength);
     });
 
-    today.setMinutes(today.getMinutes() + 1);
-    $('#timer').countdown(today)
+    sessionLength.on('change', function(e)
+    {
+        Pomodoro.setSession(sessionLength.val());
+        console.log(Pomodoro.sessionLength);
+    });
+
+    //today.setMinutes(today.getMinutes() + Pomodoro.sessionLength);
+
+    timer.countdown(new Date().setMinutes(new Date().getMinutes() + Pomodoro.sessionLength))
         .on('update.countdown', function (event)
         {
-            $('#timer').text(event.strftime('%-H h %M min %S sec'));
+            $('.timer').text(event.strftime('%M:%S'));
         })
         .on('finish.countdown', function (event)
         {
             audio.play();
+        })
+        .on('stop.countdown', function(event)
+        {
+            console.log("stopped");
         });
+
+    //timer.countdown('pause');
+
+    $('#pause').on('click', function()
+    {
+        timer.countdown('pause');
+    });
+
+    $('#resume').on('click', function()
+    {
+        timer.countdown('resume');
+    });
+
+
 });
