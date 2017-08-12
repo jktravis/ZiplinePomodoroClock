24df6747865547678fbc5d44172792c83d894cca
jktravis <joshua.travis@gmail.com>
2017-08-11 23:08:42 -0400

    Added ForkMe banner.

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import {
 import './App.css';
 import TimeDisplay from './TimeDisplay';
 import NumberField from './NumberField';
+import ForkMe from './ForkMe';
 import { convertMinToMilli, formatTime, getProgressBarValue } from './utilities';
 import Timer from './timer';
 
@@ -129,62 +130,65 @@ class App extends Component {
   render() {
     const {currentTimeDisplay, breakLength, sessionLength, progressPercent, isRunning, onBreak} = this.state;
     return (
-      <Grid fluid>
-        <Row>
-          <Col lg={2} lgPush={4} sm={2} smPush={3}>
-            <NumberField labelRef="breakLength"
-                         labelName="Break Length"
-                         changeFn={this.handleBreakChange}
-                         value={breakLength}
-            />
-          </Col>
-
-          <Col lg={2} lgPush={4} sm={2} smPush={4}>
-            <NumberField labelRef="sessionLength"
-                         labelName="Session Length"
-                         changeFn={this.handleSessionChange}
-                         value={sessionLength}
-            />
-          </Col>
-        </Row>
-
-        <Row>
-          <Col lg={4} lgPush={4} sm={5} smPush={3}>
-            <Well>
-              <TimeDisplay>
-                {currentTimeDisplay}
-              </TimeDisplay>
-            </Well>
-          </Col>
-        </Row>
-
-        <Row>
-          <Col lg={4} lgPush={4} sm={5} smPush={3}>
-            <div className="text-center">
-              <ProgressBar now={progressPercent}
-                           label={`${progressPercent}%`}
-                           bsStyle={onBreak ? 'warning' : null}
+      <div>
+        <ForkMe/>
+        <Grid fluid>
+          <Row>
+            <Col lg={2} lgPush={4} sm={2} smPush={3}>
+              <NumberField labelRef="breakLength"
+                           labelName="Break Length"
+                           changeFn={this.handleBreakChange}
+                           value={breakLength}
               />
-              <Button bsStyle="primary" onClick={this.handleReset}>
-                <FaRepeat/> Reset Timer
-              </Button> {' '}
-              <ToggleButtonGroup type="radio"
-                                 name="action"
-                                 defaultValue={isRunning}
-                                 value={isRunning}
-                                 onChange={this.handlePausePlay}
-              >
-                <ToggleButton value={0}>
-                  <FaPause/> Pause
-                </ToggleButton>
-                <ToggleButton value={1}>
-                  <FaPlay/> Play/Resume
-                </ToggleButton>
-              </ToggleButtonGroup>
-            </div>
-          </Col>
-        </Row>
-      </Grid>
+            </Col>
+
+            <Col lg={2} lgPush={4} sm={2} smPush={4}>
+              <NumberField labelRef="sessionLength"
+                           labelName="Session Length"
+                           changeFn={this.handleSessionChange}
+                           value={sessionLength}
+              />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col lg={4} lgPush={4} sm={5} smPush={3}>
+              <Well>
+                <TimeDisplay>
+                  {currentTimeDisplay}
+                </TimeDisplay>
+              </Well>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col lg={4} lgPush={4} sm={5} smPush={3}>
+              <div className="text-center">
+                <ProgressBar now={progressPercent}
+                             label={`${progressPercent}%`}
+                             bsStyle={onBreak ? 'warning' : null}
+                />
+                <Button bsStyle="primary" onClick={this.handleReset}>
+                  <FaRepeat/> Reset Timer
+                </Button> {' '}
+                <ToggleButtonGroup type="radio"
+                                   name="action"
+                                   defaultValue={isRunning}
+                                   value={isRunning}
+                                   onChange={this.handlePausePlay}
+                >
+                  <ToggleButton value={0}>
+                    <FaPause/> Pause
+                  </ToggleButton>
+                  <ToggleButton value={1}>
+                    <FaPlay/> Play/Resume
+                  </ToggleButton>
+                </ToggleButtonGroup>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
